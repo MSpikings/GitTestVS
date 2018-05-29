fc5456d32f9e5835b73fa80a7ade4aade2576d7c
MSpikings <martin.spikings@gmail.com>
2018-05-29 17:26:56 +0100

    dfsdfsdfs

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApplication1
         {
             Console.WriteLine("This is my first C# app");
             Console.WriteLine("this is the second C#");
+            Console.WriteLine("Bobob");
         }
     }
 }
